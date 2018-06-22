7268ae01ef4f674c14148c2c7f86f2549d024cc7
MKewaldar <marlon_kewaldar@hotmail.com>
2018-06-22 15:44:02 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,52 @@
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.testMethod(main.parse(new File("centraal_station_middelburg.xml")));
+    }
+
+    /**
+     * Reads the XML document
+     * @param file File to read
+     * @return Document object ready to be reasoned with
+     */
+    private Document parse(File file)  {
+        SAXReader reader = new SAXReader();
+        org.dom4j.Document doc = null;
+        try {
+            doc = reader.read(file);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        return doc;
+        }
+
+    /**
+     * TODO: Remove
+     * @param doc
+     */
+    private void testMethod(Document doc) {
+        Element root = doc.getRootElement();
+        System.out.println("Root element: " + root.getQualifiedName());
+        System.out.println();
+
+        System.out.println(doc.selectSingleNode("/rdf:RDF/swivt:Subject/rdfs:label").getName() + ": " +
+                doc.selectSingleNode("/rdf:RDF/swivt:Subject/rdfs:label").getStringValue());
+
+        String str = doc.selectSingleNode("/rdf:RDF/swivt:Subject/property:Context/@rdf:resource").getStringValue();
+
+        str = str.substring(str.lastIndexOf('/')+1 );
+
+        System.out.println(doc.selectSingleNode("/rdf:RDF/swivt:Subject/property:Context").getName() + ": " + str );
+
+        System.out.println(doc.selectSingleNode("/rdf:RDF/swivt:Subject/property:Transport").getName() + ": " +
+        doc.selectSingleNode("/rdf:RDF/swivt:Subject/property:Transport").getStringValue());
+
+    }
+}
